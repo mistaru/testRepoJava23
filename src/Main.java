04b19b398aef10407f3067b0fe93d13ccf50d68d
argen_kasymov <mistaru@bk.ru>
2023-11-08 20:11:20 +0600

    добавил новую строку "Hello Argen Hello Argen"

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world2");
         System.out.println("Hello world3");
         System.out.println("Hello Argen");
+        System.out.println("Hello Argen Hello Argen");
     }
 }
